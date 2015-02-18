35a9dff86e6042fe3597083e21eac1c8510055a7
jugle66@hotmail.com <jugle66@hotmail.com>
2015-02-18 15:31:17 -0500

    Release v0.2.5

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('tag', function () {
   return gulp.src('./')
     .pipe(git.commit(message))
     //.pipe(git.tag(v, message))
-    .pipe(git.push('origin', 'master', '--tags'))
+    .pipe(git.push('origin', 'master', "auto-commit"))
     .pipe(gulp.dest('./'));
 });
  
