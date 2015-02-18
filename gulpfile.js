4bfc7daebba8658d46513559ad0c2bfd8dd66eec
jugle66@hotmail.com <jugle66@hotmail.com>
2015-02-18 14:43:17 -0500

    new version

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+ 
+
+ 
+// Define the key for versioning off 
+gulp.task('bump', function(){
+  gulp.src('./package.json')
+  .pipe(bump({key: "version"}))
+  .pipe(gulp.dest('./'));
+});
+ 
+
+
+
+gulp.task('prepare', function(){
+  gulp.src('./package.json')
+  .pipe(bump({key: "version"}))
+  .pipe(gulp.dest('./'));
+});
+ 
