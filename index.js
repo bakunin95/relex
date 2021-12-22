76f45179c5f2f93fb9c43c5b3ab20178844bc07e
bakunin95 <jugle66@hotmail.com>
2021-12-22 15:54:31 -0500

    test

--- index.js
+++ index.js
@@ -11,31 +11,33 @@ var resultFile = path.resolve(__dirname+'/../../app/data/result.csv')
 exports.extract = function(website_path,include_node_modules,callback) {
 	fs.writeFile(resultFile, '', function(){
 		var parentDir = path.resolve(__dirname+'/Mongodb');
-		relationsClass.getRelations(website_path,include_node_modules,function(err){	
+		relationsClass.getRelations(website_path,include_node_modules,function(err){
 	    	db.findNodesById({},function(nodes){
 	    		db.findLinks({},function(links){
-	    			callback(err,{"nodes":nodes,"links":links,"groups":libGroups.groups}); 					    
+	    			callback(err,{"nodes":nodes,"links":links,"groups":libGroups.groups});
 				});
-			});		
-		});	
-	
+			});
+		});
+
 	});
 };
+
+
 
 
 var async = require('async');
 
 exports.extract = function(website_path,include_node_modules,callback) {
 	var parentDir = path.resolve(__dirname+'/Mongodb');
-	relationsClass.getRelations(website_path,include_node_modules,function(err){	
+	relationsClass.getRelations(website_path,include_node_modules,function(err){
     	db.findNodesById({},function(nodes){
-    		async.sortBy(nodes, function(myObject, cbSortBy){					    
+    		async.sortBy(nodes, function(myObject, cbSortBy){
 			   cbSortBy(null,myObject.id);
-			}, function(err, sortedNodes){  
+			}, function(err, sortedNodes){
 	    		db.findLinks({},function(links){
-				    callback(err,{"nodes":sortedNodes,"links":links,"groups":libGroups.groups}); 
+				    callback(err,{"nodes":sortedNodes,"links":links,"groups":libGroups.groups});
 				});
 			});
-		});		
-	});	
+		});
+	});
 };
